26d78bb325302fa1b357784b28c7e007514df3ff
felipepimentab <felipe.pbernardo@gmail.com>
2025-11-05 18:09:39 -0300

    style(LogoMTH): adjust text size and font family for logo component

--- app/components/LogoMTH.vue
+++ app/components/LogoMTH.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-lg px-1.5 py-0.5 tracking-wider border border-text dark:border-text-dark flex items-center text-text dark:text-text-dark">
+  <div class="text-base px-1.5 py-0.5 tracking-wider border border-text dark:border-text-dark flex items-center text-text dark:text-text-dark font-lato">
     <span class="font-black">MIND</span>
     <span class="font-light">THE</span>
     <span class="font-black">HEADPHONE</span>
